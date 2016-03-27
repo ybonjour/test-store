68187b09395830ee4fd884f0ba0d0087a53af1c3
Yves Bonjour <yves.bonjour@gmail.com>
2016-03-28 00:31:57 +0200

    Adds correct entity attributes for Result model

--- src/main/kotlin/ch/yvu/teststore/result/Result.kt
+++ src/main/kotlin/ch/yvu/teststore/result/Result.kt
@@ -1,9 +1,12 @@
 package ch.yvu.teststore.result
 
 import ch.yvu.teststore.common.Model
+import org.springframework.data.cassandra.mapping.PrimaryKey
+import org.springframework.data.cassandra.mapping.Table
 import java.util.*
 
-data class Result(val id: UUID, val run: UUID, val test: UUID, val retryNum: Int, val passed: Boolean) : Model {
+@Table("result")
+data class Result(@PrimaryKey val id: UUID, val run: UUID, val test: UUID, val retryNum: Int, val passed: Boolean) : Model {
     override fun id(): UUID {
         return id
     }
