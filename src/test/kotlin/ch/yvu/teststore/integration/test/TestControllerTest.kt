227706c5ffe24474e87527f34856a6bd8245d21c
Yves Bonjour <yves.bonjour@gmail.com>
2016-03-28 00:45:32 +0200

    Tests if test is stored in repository with correct name

--- src/test/kotlin/ch/yvu/teststore/integration/test/TestControllerTest.kt
+++ src/test/kotlin/ch/yvu/teststore/integration/test/TestControllerTest.kt
@@ -1,11 +1,17 @@
 package ch.yvu.teststore.integration.test
 
 import ch.yvu.teststore.integration.BaseIntegrationTest
+import ch.yvu.teststore.test.Test
 import ch.yvu.teststore.test.TestRepository
 import com.jayway.restassured.RestAssured.given
+import org.hamcrest.Description
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.hasItem
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Assert
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
 import org.junit.Before
-import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 
 class TestControllerTest : BaseIntegrationTest() {
@@ -17,15 +23,31 @@ class TestControllerTest : BaseIntegrationTest() {
         testRepository.deleteAll()
     }
 
-    @Test fun createTestReturnsCorrectStatusCode() {
+    @org.junit.Test fun createTestReturnsCorrectStatusCode() {
         given().queryParam("name", "ATest").post("/tests").then().assertThat().statusCode(201)
     }
 
-    @org.junit.Test fun itCanStoreATest() {
-        val testName = "ATest"
+    @org.junit.Test fun createTestStoresTestWithCorrectName() {
+        val name = "ATest"
 
-        given().queryParam("name", testName).post("/tests")
+        given().queryParam("name", name).post("/tests")
 
-        assertEquals(1, testRepository.count())
+        val tests = testRepository.findAll()
+        assertEquals(1, tests.count())
+        assertThat(tests, hasItem(testWithName(name)))
+    }
+
+    private fun testWithName(name: String) = object : TypeSafeMatcher<Test>() {
+        override fun matchesSafely(item: Test?): Boolean {
+            if(item == null) return false
+
+            return item.name == name
+        }
+
+        override fun describeTo(description: Description?) {
+            if(description == null) return
+
+            description.appendText("Test with name ${name}")
+        }
     }
 }
