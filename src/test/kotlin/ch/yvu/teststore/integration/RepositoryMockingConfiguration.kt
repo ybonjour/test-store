6b758129c0ebc61d6428642d821f3912bb5915ac
Yves Bonjour <yves.bonjour@gmail.com>
2016-03-28 14:09:20 +0200

    Removes TestController

--- src/test/kotlin/ch/yvu/teststore/integration/RepositoryMockingConfiguration.kt
+++ src/test/kotlin/ch/yvu/teststore/integration/RepositoryMockingConfiguration.kt
@@ -2,11 +2,9 @@ package ch.yvu.teststore.integration
 
 import ch.yvu.teststore.integration.run.ListBackedResultRepository
 import ch.yvu.teststore.integration.run.ListBackedRunRepository
-import ch.yvu.teststore.integration.test.ListBackedTestRepository
 import ch.yvu.teststore.integration.testsuite.ListBackedTestSuiteRepository
 import ch.yvu.teststore.result.ResultRepository
 import ch.yvu.teststore.run.RunRepository
-import ch.yvu.teststore.test.TestRepository
 import ch.yvu.teststore.testsuite.TestSuiteRepository
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration
@@ -30,11 +28,6 @@ open class RepositoryMockingConfiguration {
     @Bean
     open fun testSuiteRepository(): TestSuiteRepository {
         return ListBackedTestSuiteRepository(ListBackedRepository())
-    }
-
-    @Bean
-    open fun testRepository(): TestRepository {
-        return ListBackedTestRepository(ListBackedRepository())
     }
 
     @Bean
