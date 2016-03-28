316e00b28c5e820c31987379b01ab4b32f6d22e6
Yves Bonjour <yves.bonjour@gmail.com>
2016-03-28 10:56:45 +0200

    Implements logic to save an empty run

--- src/main/kotlin/ch/yvu/teststore/insert/InsertService.kt
+++ src/main/kotlin/ch/yvu/teststore/insert/InsertService.kt
@@ -1,18 +1,30 @@
 package ch.yvu.teststore.insert
 
+import ch.yvu.teststore.insert.dto.RunDto
 import ch.yvu.teststore.insert.dto.TestSuiteDto
+import ch.yvu.teststore.run.Run
+import ch.yvu.teststore.run.RunRepository
 import ch.yvu.teststore.testsuite.TestSuite
 import ch.yvu.teststore.testsuite.TestSuiteRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.*
 import java.util.UUID.randomUUID
 
 @Service
-class InsertService @Autowired constructor(val testSuiteRepository: TestSuiteRepository) {
+class InsertService @Autowired constructor(
+        val testSuiteRepository: TestSuiteRepository,
+        val runRepository: RunRepository) {
 
     fun insertTestSuite(testSuiteDto: TestSuiteDto): TestSuite {
         val testSuite = TestSuite(randomUUID(), testSuiteDto.name)
         testSuiteRepository.save(testSuite)
         return testSuite
+    }
+
+    fun insertRun(runDto: RunDto, testSuiteId: UUID):Run{
+        val run = Run(randomUUID(), testSuiteId, runDto.revision)
+        runRepository.save(run)
+        return run
     }
 }
