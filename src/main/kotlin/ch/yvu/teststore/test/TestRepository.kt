05f3bbbf4a881233ec2701f084b8a181bffa70c0
Yves Bonjour <yves.bonjour@gmail.com>
2016-03-28 14:11:14 +0200

    Removes TestRepository

--- src/main/kotlin/ch/yvu/teststore/test/TestRepository.kt
+++
@@ -1,7 +0,0 @@
-package ch.yvu.teststore.test
-
-import org.springframework.data.cassandra.repository.CassandraRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface TestRepository : CassandraRepository<Test>
