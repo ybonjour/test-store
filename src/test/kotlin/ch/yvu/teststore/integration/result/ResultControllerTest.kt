a7f1d7cc326b3b3643b3e30ca1eeb063999e034a
Yves Bonjour <yves.bonjour@gmail.com>
2016-03-28 11:30:43 +0200

    Extracts ResultMatchers from ResultControllerTest

--- src/test/kotlin/ch/yvu/teststore/integration/result/ResultControllerTest.kt
+++ src/test/kotlin/ch/yvu/teststore/integration/result/ResultControllerTest.kt
@@ -1,18 +1,15 @@
 package ch.yvu.teststore.integration.result
 
 import ch.yvu.teststore.integration.BaseIntegrationTest
-import ch.yvu.teststore.result.Result
+import ch.yvu.teststore.matchers.ResultMatchers.resultWith
 import ch.yvu.teststore.result.ResultRepository
 import com.jayway.restassured.RestAssured.given
-import org.hamcrest.Description
 import org.hamcrest.Matchers.*
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
-import java.util.*
 import java.util.UUID.randomUUID
 
 class ResultControllerTest : BaseIntegrationTest() {
@@ -57,19 +54,4 @@ class ResultControllerTest : BaseIntegrationTest() {
                 .post("/results")
         .then().assertThat().body("id", not(isEmptyOrNullString()))
     }
-
-    private fun resultWith(run: UUID, test: UUID, retryNum: Int, passed: Boolean) = object : TypeSafeMatcher<Result>() {
-        override fun describeTo(description: Description?) {
-            if (description == null) return
-            description.appendText("Result with run ${run}, test ${test}, retryNum ${retryNum}, passed ${passed}")
-        }
-
-        override fun matchesSafely(item: Result?): Boolean {
-            if (item == null) return false
-
-            return item.run == run && item.test == test && item.retryNum == retryNum && item.passed == passed
-        }
-
-    }
-
 }
