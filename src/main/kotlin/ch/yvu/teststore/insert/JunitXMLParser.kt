62d42f4e9ce06c6442768a69c191ea93bef867af
Yves Bonjour <yves.bonjour@gmail.com>
2016-04-03 10:38:04 +0200

    Adds logic to parse Junit XML to ResultDtos

---
+++ src/main/kotlin/ch/yvu/teststore/insert/JunitXMLParser.kt
@@ -0,0 +1,60 @@
+package ch.yvu.teststore.insert
+
+import ch.yvu.teststore.insert.dto.ResultDto
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+import javax.xml.parsers.DocumentBuilderFactory
+import kotlin.text.Charsets.UTF_8
+
+object JunitXMLParser {
+    fun parse(xml: String): List<ResultDto> {
+        val testResultNodes = testResultNodes(xml)
+
+        var resultDtos = emptyList<ResultDto>()
+        for (i in 0..testResultNodes.length - 1) {
+            val testResult = TestResultNode(testResultNodes.item(i))
+            if (testResult.skipped) continue
+
+            resultDtos += ResultDto(
+                    testName = "${testResult.classname}#${testResult.name}",
+                    retryNum = 0,
+                    passed = testResult.passed,
+                    durationSeconds = testResult.durationMs
+            )
+        }
+
+        return resultDtos
+    }
+
+    private fun testResultNodes(xml: String): NodeList {
+        val factory = DocumentBuilderFactory.newInstance()
+        val builder = factory.newDocumentBuilder()
+
+        val document = builder.parse(xml.byteInputStream(UTF_8))
+
+        return document.getElementsByTagName("testcase")
+    }
+
+    private class TestResultNode(val node: Node) {
+
+        val name = node.attributes.getNamedItem("name").nodeValue
+
+        val classname = node.attributes.getNamedItem("classname").nodeValue
+
+        val durationMs = Math.round(node.attributes.getNamedItem("time").nodeValue.toDouble() * 1000)
+
+        val passed = !hasChildWithName("failure", "error")
+
+        val skipped = hasChildWithName("skipped")
+
+        private fun hasChildWithName(vararg names: String): Boolean {
+            val children = node.childNodes
+            for (i in 0..children.length - 1) {
+                val child = children.item(i)
+                if (names.contains(child.nodeName)) return true
+            }
+
+            return false
+        }
+    }
+}
---
+++ src/test/kotlin/ch/yvu/teststore/insert/JunitXmlConverterTest.kt
@@ -0,0 +1,106 @@
+package ch.yvu.teststore.insert
+
+import ch.yvu.teststore.insert.dto.ResultDto
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class JunitXmlParserTest {
+
+    @Test
+    fun canParsePassedTest() {
+        val xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<testsuites>\n    <testsuite errors=\"0\" skipped=\"0\" tests=\"1\" failures=\"0\" timestamp=\"2016-03-31T17:51:02\">\n        <testcase classname=\"ch.yvu.teststore.MyTestClass\" name=\"myTest\" time=\"0.006\"/>\n    </testsuite>\n</testsuites>"
+
+        val parsed = JunitXMLParser.parse(xml)
+
+        assertEquals(listOf(
+                ResultDto(
+                        testName = "ch.yvu.teststore.MyTestClass#myTest",
+                        retryNum = 0,
+                        passed = true,
+                        durationSeconds = 6
+                )
+        ), parsed)
+    }
+
+    @Test
+    fun canParseFailedTest() {
+        val xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<testsuites>\n    <testsuite errors=\"0\" skipped=\"0\" tests=\"1\" failures=\"0\" timestamp=\"2016-03-31T17:51:02\">\n        <testcase classname=\"ch.yvu.teststore.MyTestClass\" name=\"myTest\" time=\"0.006\">\n            <failure />\n        </testcase>\n    </testsuite>\n</testsuites>"
+
+        val parsed = JunitXMLParser.parse(xml)
+
+        assertEquals(listOf(
+                ResultDto(
+                        testName = "ch.yvu.teststore.MyTestClass#myTest",
+                        retryNum = 0,
+                        passed = false,
+                        durationSeconds = 6
+                )
+        ), parsed)
+    }
+
+    @Test
+    fun canParseErroredTest() {
+        val xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<testsuites>\n    <testsuite errors=\"0\" skipped=\"0\" tests=\"1\" failures=\"0\" timestamp=\"2016-03-31T17:51:02\">\n        <testcase classname=\"ch.yvu.teststore.MyTestClass\" name=\"myTest\" time=\"0.006\">\n            <error />\n        </testcase>\n    </testsuite>\n</testsuites>"
+
+        val parsed = JunitXMLParser.parse(xml)
+
+        assertEquals(listOf(
+                ResultDto(
+                        testName = "ch.yvu.teststore.MyTestClass#myTest",
+                        retryNum = 0,
+                        passed = false,
+                        durationSeconds = 6
+                )
+        ), parsed)
+    }
+
+    @Test
+    fun ignoresSkippedTest() {
+        val xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<testsuites>\n    <testsuite errors=\"0\" skipped=\"0\" tests=\"1\" failures=\"0\" timestamp=\"2016-03-31T17:51:02\">\n        <testcase classname=\"ch.yvu.teststore.MyTestClass\" name=\"myTest\" time=\"0.006\">\n            <skipped />\n        </testcase>\n    </testsuite>\n</testsuites>"
+
+        val parsed = JunitXMLParser.parse(xml)
+
+        assertEquals(emptyList<ResultDto>(), parsed)
+    }
+
+    @Test
+    fun canParseMultipleTestSuites() {
+        val xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<testsuites>\n    <testsuite errors=\"0\" skipped=\"0\" tests=\"1\" failures=\"0\" timestamp=\"2016-03-31T17:51:02\">\n        <testcase classname=\"ch.yvu.teststore.MyTestClass\" name=\"myTest\" time=\"0.006\" />\n    </testsuite>\n    <testsuite errors=\"0\" skipped=\"0\" tests=\"1\" failures=\"0\" timestamp=\"2016-03-31T17:51:02\">\n        <testcase classname=\"ch.yvu.teststore.MyTestClass2\" name=\"myTest\" time=\"0.01\" />\n    </testsuite>\n</testsuites>"
+
+        val parsed = JunitXMLParser.parse(xml)
+
+        assertEquals(listOf(
+                ResultDto(
+                        testName = "ch.yvu.teststore.MyTestClass#myTest",
+                        retryNum = 0,
+                        passed = true,
+                        durationSeconds = 6
+                ),
+                ResultDto(
+                        testName = "ch.yvu.teststore.MyTestClass2#myTest",
+                        retryNum = 0,
+                        passed = true,
+                        durationSeconds = 10
+                )
+        ), parsed)
+    }
+
+    @Test
+    fun canParseEmptyTestSuite() {
+        val xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<testsuites>\n    <testsuite errors=\"0\" skipped=\"0\" tests=\"1\" failures=\"0\" timestamp=\"2016-03-31T17:51:02\" />\n</testsuites>"
+
+        val parsed = JunitXMLParser.parse(xml)
+
+        assertEquals(emptyList<ResultDto>(), parsed)
+    }
+
+    @Test
+    fun canParsNoTestSuites() {
+        val xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<testsuites />"
+
+        val parsed = JunitXMLParser.parse(xml)
+
+        assertEquals(emptyList<ResultDto>(), parsed)
+    }
+
+}
