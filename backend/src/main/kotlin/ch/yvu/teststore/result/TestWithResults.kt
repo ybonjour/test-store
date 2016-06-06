56f11ec6b19369ac7ec3e6447f9c54992a694a0f
Yves Bonjour <yves.bonjour@gmail.com>
2016-06-06 13:49:50 +0200

    Added TestWithResults that represents a single test in a run with potential retries

---
+++ backend/src/main/kotlin/ch/yvu/teststore/result/TestWithResults.kt
@@ -0,0 +1,20 @@
+package ch.yvu.teststore.result
+
+class TestWithResults(val testName: String, val results: List<Result> = emptyList()) {
+    enum class TestResult {
+        PASSED, FAILED, RETRIED
+    }
+
+    fun addResult(result: Result): TestWithResults {
+        return TestWithResults(testName, results + result)
+    }
+
+    fun getTestResult(): TestResult {
+        if (results.isEmpty()) return TestResult.PASSED
+        val lastResult = results.sortedBy { it.retryNum }.last()
+
+        if (lastResult.isFailed()) return TestResult.FAILED
+        else if (lastResult.isRetry()) return TestResult.RETRIED
+        else return TestResult.PASSED
+    }
+}
---
+++ backend/src/test/kotlin/ch/yvu/teststore/result/TestWithResultsTest.kt
@@ -0,0 +1,59 @@
+package ch.yvu.teststore.result
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.util.UUID.randomUUID
+
+class TestWithResultsTest {
+    companion object {
+        val runId = randomUUID()
+        val passedResultTest1 = Result(runId, "test1", 0, true, 20);
+        val failedResultTest1 = Result(runId, "test1", 0, false, 20);
+    }
+
+    @Test fun onePassedResultIsPassed() {
+        val testWithResults = TestWithResults(passedResultTest1.testName!!)
+                .addResult(passedResultTest1)
+
+        val testResult = testWithResults.getTestResult()
+
+        assertEquals(TestWithResults.TestResult.PASSED, testResult)
+    }
+
+    @Test fun oneFailedResultIsFailed() {
+        val testWithResults = TestWithResults(failedResultTest1.testName!!)
+                .addResult(failedResultTest1)
+
+        val testResult = testWithResults.getTestResult()
+
+        assertEquals(TestWithResults.TestResult.FAILED, testResult)
+    }
+
+    @Test fun noResultsIsPassed() {
+        val testWithResults = TestWithResults("myTest")
+
+        val testResult = testWithResults.getTestResult()
+
+        assertEquals(TestWithResults.TestResult.PASSED, testResult)
+    }
+
+    @Test fun retryPassedIsRetried() {
+        val passedRetry = Result(failedResultTest1.run, failedResultTest1.testName, 1, true, 23);
+        val testWithResults = TestWithResults(failedResultTest1.testName!!)
+                .addResult(failedResultTest1).addResult(passedRetry)
+
+        val testResult = testWithResults.getTestResult()
+
+        assertEquals(TestWithResults.TestResult.RETRIED, testResult)
+    }
+
+    @Test fun retryFailedIsFailed() {
+        val failedRetry = Result(failedResultTest1.run, failedResultTest1.testName, 1, false, 23);
+        val testWithResults = TestWithResults(failedResultTest1.testName!!)
+                .addResult(failedResultTest1).addResult(failedRetry)
+
+        val testResult = testWithResults.getTestResult()
+
+        assertEquals(TestWithResults.TestResult.FAILED, testResult)
+    }
+}
