3977eebd97d6b6256405155d43dfe6d14b4e58e6
Yves Bonjour <yves.bonjour@gmail.com>
2016-07-04 19:47:33 +0200

    Adds more content to test results diff view

--- web/www/app/test-result/diff/test-result-diff.component.ts
+++ web/www/app/test-result/diff/test-result-diff.component.ts
@@ -3,15 +3,20 @@ import {RouteParams} from "angular2/router";
 import {TestResultService} from "../test-result.service";
 import {TestWithResults} from "../test-with-results";
 import {TestResultsComponent} from "../test-results.component";
+import {TestResultsHeaderComponent} from "../header/test-results-header.component";
 
 @Component({
     templateUrl: 'app/test-result/diff/test-result-diff.html',
-    directives: [TestResultsComponent]
+    styleUrls: ['app/test-result/diff/test-result-diff.css'],
+    directives: [TestResultsComponent, TestResultsHeaderComponent]
 })
 export class TestResultDiffComponent implements OnInit {
 
     runId:string;
     results:{string:TestWithResults[]} = {};
+    numPassedResults:number = 0;
+    numRetriedResults:number = 0;
+    numFailedResults:number = 0;
     errorMessage:string;
 
 
@@ -23,8 +28,29 @@ export class TestResultDiffComponent implements OnInit {
     ngOnInit():any {
         this.runId = this._routeParams.get('run_id');
         this._testResultService.getResultsDiff(this.runId).subscribe(
-            results => this.results = results,
+            results => this.extractResults(results),
             error => this.errorMessage = <any>error
         );
+    }
+
+    extractResults(results:{string:TestWithResults[]}) {
+        this.results = results;
+        this.numPassedResults = this.numResultsForCategory(results, "NEW_PASSED")
+            + this.numResultsForCategory(results, "FIXED")
+            + this.numResultsForCategory(results, "STILL_PASSING");
+
+        this.numRetriedResults = this.numResultsForCategory(results, "NEW_RETRIED")
+            + this.numResultsForCategory(results, "RETRIED_AFTER_PASSED")
+            + this.numResultsForCategory(results, "RETRIED_AFTER_FAILED")
+            + this.numResultsForCategory(results, "STILL_RETRIED");
+
+        this.numFailedResults = this.numResultsForCategory(results, "NEW_FAILED")
+            + this.numResultsForCategory(results, "BROKE")
+            + this.numResultsForCategory(results, "STILL_FAILING");
+    }
+
+    private numResultsForCategory(results:{string:TestWithResults[]}, category:string) {
+        if (!(category in results)) return 0;
+        return results[category].length;
     }
 }
