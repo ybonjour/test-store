6a7d44cd047b8454caf9464e79d72607dd701cc1
Yves Bonjour <yves.bonjour@gmail.com>
2016-07-19 20:47:35 +0200

    Adds /testsuites/{testSuite}/statistics/{testName} endpoint

--- backend/src/main/kotlin/ch/yvu/teststore/statistics/StatisticsController.kt
+++ backend/src/main/kotlin/ch/yvu/teststore/statistics/StatisticsController.kt
@@ -1,10 +1,14 @@
 package ch.yvu.teststore.statistics
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.http.HttpStatus.OK
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod.GET
 import org.springframework.web.bind.annotation.RestController
+import java.net.URLDecoder
 import java.util.*
 
 @RestController
@@ -15,6 +19,17 @@ class StatisticsController @Autowired constructor(val testStatisticsRepository: 
             @PathVariable testSuite: UUID
     ): List<TestStatistics> {
         return testStatisticsRepository.findAllByTestSuite(testSuite)
+    }
+
+    @RequestMapping(method = arrayOf(GET), value = "/testsuites/{testSuite}/statistics/{testName}")
+    fun getStatisticsByTestSuiteAndTestName(
+            @PathVariable testSuite: UUID,
+            @PathVariable testName: String): ResponseEntity<TestStatistics> {
+        val testNameDecoded = URLDecoder.decode(testName, "UTF-8")
+        val testStatistics = testStatisticsRepository.findByTestSuiteAndTestName(testSuite, testNameDecoded)
+
+        if(testStatistics == null) return ResponseEntity(NOT_FOUND)
+        else return ResponseEntity(testStatistics, OK)
     }
 
 }
--- backend/src/test/kotlin/ch/yvu/teststore/integration/statistics/StatisticsControllerTest.kt
+++ backend/src/test/kotlin/ch/yvu/teststore/integration/statistics/StatisticsControllerTest.kt
@@ -8,6 +8,7 @@ import org.hamcrest.Matchers.equalTo
 import org.junit.Before
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
+import java.net.URLEncoder
 import java.util.UUID.randomUUID
 
 
@@ -33,5 +34,27 @@ class StatisticsControllerTest : BaseIntegrationTest() {
                 .statusCode(200)
                 .body("[0].testName", equalTo("myTest1"))
                 .body("[1].testName", equalTo("myTest2"))
+    }
+
+    @Test fun getStatisticsByTestSuiteAndTestNameReturnsStatistics() {
+        val testSuiteId = randomUUID()
+        val statistics = TestStatistics(testSuiteId, "MyTest#myTest", 1, 0)
+        statisticsRepository.save(statistics)
+        val encodedTestName = URLEncoder.encode(statistics.testName, "UTF-8")
+
+        given()
+                .get("/testsuites/$testSuiteId/statistics/$encodedTestName")
+        .then()
+                .statusCode(200)
+                .body("testName", equalTo(statistics.testName))
+    }
+
+    @Test fun getStatisticsByTestSuiteAndTestNameReturns404IfNoStatisicsIsFound() {
+        val testSuiteId = randomUUID()
+
+        given()
+            .get("/testsuites/$testSuiteId/statistics/myTest")
+        .then()
+            .statusCode(404)
     }
 }
