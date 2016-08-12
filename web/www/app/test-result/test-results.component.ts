148005a26bf576451cf97ed01f34e580223b7b06
Yves Bonjour <yves.bonjour@gmail.com>
2016-08-12 10:47:07 +0200

    Adds link to test details from test-results

--- web/www/app/test-result/test-results.component.ts
+++ web/www/app/test-result/test-results.component.ts
@@ -1,14 +1,22 @@
-import {Component, Input} from 'angular2/core'
+import {Component, Input, OnInit} from "angular2/core";
 import {TestWithResults} from "./test-with-results";
 import {StacktraceComponent} from "./stacktrace/stacktrace.component";
+import {RouteParams, ROUTER_DIRECTIVES} from "angular2/router";
 
 @Component({
     selector: 'test-results',
     templateUrl: 'app/test-result/test-results.html',
     styleUrls: ['app/test-result/test-results.css'],
-    directives: [StacktraceComponent]
+    directives: [StacktraceComponent, ROUTER_DIRECTIVES]
 })
-export class TestResultsComponent {
+export class TestResultsComponent implements OnInit {
     @Input() results: TestWithResults[];
     @Input() type: string;
+    testSuiteId: string;
+
+    constructor(private _routeParams: RouteParams) {}
+
+    ngOnInit():any {
+        this.testSuiteId = this._routeParams.get('testsuite_id');
+    }
 }
