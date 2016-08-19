37fe23bce32ceb05b371b588d151d47ac8a451d0
Yves Bonjour <yves.bonjour@gmail.com>
2016-08-19 08:24:59 +0200

    Adds basic duration chart to test-details

--- web/www/app/test/test-details.component.ts
+++ web/www/app/test/test-details.component.ts
@@ -4,15 +4,21 @@ import {TestResultService} from "../test-result/test-result.service";
 import {TestResult} from "../test-result/test-result";
 import {TestStatisticsService} from "../statistics/test-statistics.service";
 import {TestStatistics} from "../statistics/test-statistics";
+import {FORM_DIRECTIVES} from "@angular/forms";
+import {CORE_DIRECTIVES, NgClass} from "@angular/common";
+import {CHART_DIRECTIVES} from "ng2-charts/ng2-charts";
 
 @Component({
     templateUrl: 'app/test/test-details.html',
-    styleUrls: ['app/test/test-details.css']
+    styleUrls: ['app/test/test-details.css'],
+    directives: [CHART_DIRECTIVES, NgClass, CORE_DIRECTIVES, FORM_DIRECTIVES]
 })
 export class TestDetailsComponent implements OnInit{
     testSuiteId: string;
     testName: string;
     results: TestResult[] = [];
+    durations: number[];
+    labels: string[];
     statistics: TestStatistics;
     errorMessage: string;
 
@@ -24,6 +30,7 @@ export class TestDetailsComponent implements OnInit{
     ngOnInit():any {
         this.testSuiteId = this._routeParams.get('testsuite_id');
         this.testName = this._routeParams.get('test_name');
+
 
         this._testResultService.getResultsByTestSuiteAndTestName(this.testSuiteId, this.testName).subscribe(
             results => this.handleResults(results),
@@ -38,5 +45,11 @@ export class TestDetailsComponent implements OnInit{
 
     private handleResults(results:TestResult[]) {
         this.results = results;
+        this.durations = [];
+        this.labels = [];
+        for(let index in results) {
+            this.durations.push(results[index].durationMillis);
+            this.labels.push(index);
+        }
     }
 }
