4298810f11fcb680c67da9f88fb903ff1bfeb694
Yves Bonjour <yves.bonjour@ergon.ch>
2016-10-08 15:42:33 +0200

    Makes JsonPageExtractor.extractFromJson method static

--- web/www/app/common/json-page-extractor.spec.ts
+++ web/www/app/common/json-page-extractor.spec.ts
@@ -10,21 +10,15 @@ describe('PageResponseExtractor', () => {
         nextPage: "abc-123"
     };
 
-    var pageResponseExtractor: JsonPageExtractor<Run>;
-
-    beforeEach(() => {
-        pageResponseExtractor = new JsonPageExtractor<Run>();
-    });
-
     it('should extract nextPage parameter', () => {
-        let result = pageResponseExtractor.extractFromJson(pageJson, () => new Run());
+        let result = JsonPageExtractor.extractFromJson(pageJson, () => new Run());
 
         expect(result.nextPage).toBe(pageJson.nextPage)
     });
 
     it('should convert page results', () => {
         let run = new Run();
-        let result = pageResponseExtractor.extractFromJson(pageJson, (json) => { return run; });
+        let result = JsonPageExtractor.extractFromJson(pageJson, (json) => { return run; });
 
         expect(result.results).toEqual([run]);
     });
--- web/www/app/common/json-page-extractor.ts
+++ web/www/app/common/json-page-extractor.ts
@@ -1,7 +1,7 @@
 import {Page} from "./page";
 
-export class JsonPageExtractor<T> {
-    extractFromJson(json: any, elementExtractor: (any) => T):Page<T> {
+export class JsonPageExtractor {
+    static extractFromJson<T>(json: any, elementExtractor: (any) => T):Page<T> {
         let page = new Page<T>();
         page.nextPage = json.nextPage;
         page.results = this.extractElements(json.results, elementExtractor);
@@ -9,7 +9,7 @@ export class JsonPageExtractor<T> {
         return page;
     }
 
-    private extractElements(elementsJson: any, elementExtractor: (any) => T): T[] {
+    private static extractElements<T>(elementsJson: any, elementExtractor: (any) => T): T[] {
         let elements = [];
         for(let elementJson of elementsJson) {
             let element = elementExtractor(elementJson);
