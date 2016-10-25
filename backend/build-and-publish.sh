399a5772aa25b279b174380325b643a9e21093c2
Yves Bonjour <yves.bonjour@ergon.ch>
2016-10-25 17:14:29 +0200

    Use script path to build and publish docker images

--- backend/build-and-publish.sh
+++ backend/build-and-publish.sh
@@ -6,8 +6,8 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 version=$(cat $dir/CURRENT_DISPLAY_VERSION)
 
-docker build --tag ybonjour/test-store-backend:$version .
-docker build --tag ybonjour/test-store-backend .
+docker build --tag ybonjour/test-store-backend:$version $dir
+docker build --tag ybonjour/test-store-backend $dir
 
 docker push ybonjour/test-store-backend:$version
 docker push ybonjour/test-store-backend
--- web/build-and-publish.sh
+++ web/build-and-publish.sh
@@ -6,8 +6,8 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 version=$(cat $dir/CURRENT_DISPLAY_VERSION)
 
-docker build --tag ybonjour/test-store-web:$version .
-docker build --tag ybonjour/test-store-web .
+docker build --tag ybonjour/test-store-web:$version $dir
+docker build --tag ybonjour/test-store-web $dir
 
 docker push ybonjour/test-store-web:$version
 docker push ybonjour/test-store-web
