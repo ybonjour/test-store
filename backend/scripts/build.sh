11445e65489beac67eb1be9cae6ef0b8c05f064b
Yves Bonjour <yves.bonjour@ergon.ch>
2016-10-26 11:33:26 +0200

    Adds build script for backend

---
+++ backend/scripts/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+root=$dir/../..
+
+docker run -v $root/:/workspace --rm java:8-jdk bash -ce "(cd workspace/backend; ../gradlew assemble)"
