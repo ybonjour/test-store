a447d25e14122bbbf99e20e0a5152123ce7e532c
Yves Bonjour <yves.bonjour@ergon.ch>
2016-10-26 09:15:10 +0200

    Move backend scripts to backend folder

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -6,11 +6,11 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 # Build backend
 docker run -v $dir/../:/workspace --rm java:8-jdk bash -ce "(cd workspace; ./gradlew assemble)"
-docker build -t test-store-backend -f $dir/../backend/Dockerfile $dir/..
+docker build -t test-store-backend $dir/../backend
 
 # Build web
 docker run -v $dir/../web/www:/www --rm node bash -ce "(cd /www; npm install; npm run tsc)"
 docker build -t test-store-web $dir/../web
 
 # Build migration
-docker build -t test-store-migration $dir/..
+docker build -t test-store-migration $dir/../migration
