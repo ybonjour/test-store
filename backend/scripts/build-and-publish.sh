577e76d70166772037916ca02272b847f0d8cf0c
Yves Bonjour <yves.bonjour@ergon.ch>
2016-10-26 09:24:56 +0200

    Moves migration/build-and-publish.sh to script folder

---
+++ backend/scripts/build-and-publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+root=$dir/..
+
+image=ybonjour/test-store-backend
+version=$(cat $root/CURRENT_DISPLAY_VERSION)
+
+docker build --tag $image:$version $root
+docker build --tag $image $root
+
+docker push $image:$version
+docker push $image
+
+
+
