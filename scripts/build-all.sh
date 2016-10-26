e5c6ef4847595f9b6bab28c1c1efb1bf1bbbc659
Yves Bonjour <yves.bonjour@ergon.ch>
2016-10-26 09:27:55 +0200

    Simplify build-all script

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -3,14 +3,15 @@
 set -e
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+root=$dir/..
 
 # Build backend
-docker run -v $dir/../:/workspace --rm java:8-jdk bash -ce "(cd workspace; ./gradlew assemble)"
-docker build -t test-store-backend $dir/../backend
+docker run -v $root/:/workspace --rm java:8-jdk bash -ce "(cd workspace; ./gradlew assemble)"
+docker build -t test-store-backend $root/backend
 
 # Build web
-docker run -v $dir/../web/www:/www --rm node bash -ce "(cd /www; npm install; npm run tsc)"
-docker build -t test-store-web $dir/../web
+docker run -v $root/web/www:/www --rm node bash -ce "(cd /www; npm install; npm run tsc)"
+docker build -t test-store-web $root/web
 
 # Build migration
-docker build -t test-store-migration $dir/../migration
+docker build -t test-store-migration $root/migration
