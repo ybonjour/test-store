ffbd1943d4f42bfdbe38d58a080b2cd0ceac9ce0
Yves Bonjour <yves.bonjour@ergon.ch>
2016-10-27 14:29:05 +0200

    Fixes history filter by using correct argument types

--- web/www/app/history/history-filter.pipe.ts
+++ web/www/app/history/history-filter.pipe.ts
@@ -1,11 +1,10 @@
 import {Pipe, PipeTransform} from "@angular/core";
 import {HistoryTestName} from "./history-test-name";
 
-@Pipe({ name: 'filter' })
+@Pipe({ name: 'historyFilterPassed' })
 export class HistoryFilter implements PipeTransform {
-    transform(list: HistoryTestName[], applyFilter: string) {
-        let applyFilterBool = applyFilter == 'true';
-        if(!applyFilterBool) return list;
-        return list.filter((item) => {return !item.allTestsPassed});
+    transform(list: HistoryTestName[], applyFilter: boolean) {
+        if(!applyFilter) return list;
+        return list.filter((item) => {return false});
     }
 }
