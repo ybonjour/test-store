9d0d38ed8933a6cdf77a999958327ad022de010b
Yves Bonjour <yves.bonjour@gmail.com>
2016-11-10 20:19:46 +0100

    Fixes seconds in duration to 3 fraction digits

--- web/www/app/duration/duration.component.ts
+++ web/www/app/duration/duration.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, Input} from "@angular/core";
 
 @Component({
     selector: 'duration',
@@ -9,17 +9,19 @@ export class DurationComponent {
 
     printDuration() {
         var seconds = this.durationMillis / 1000;
-        if(seconds == 0) return "0s";
+        if(seconds == 0) return "0.000s";
         let hours = Math.floor(seconds / 3600);
         seconds = seconds % 3600;
         let minutes = Math.floor(seconds / 60);
         seconds = seconds % 60;
 
-        return DurationComponent.printduration(hours, "h") + " " + DurationComponent.printduration(minutes, "m") + " " + DurationComponent.printduration(seconds, "s");
+        return DurationComponent.printduration(hours, "h") + " "
+            + DurationComponent.printduration(minutes, "m") + " "
+            + DurationComponent.printduration(seconds, "s", 3);
     }
 
-    private static printduration(amount: number, unit: String): String {
+    private static printduration(amount: number, unit: String, fractionDigits: number = 0): String {
         if(amount == 0) return "";
-        else return "" + amount + unit;
+        else return "" + amount.toFixed(fractionDigits) + unit;
     }
 }
