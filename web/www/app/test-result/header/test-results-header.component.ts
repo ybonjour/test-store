30ba222359194823639c226f924507ff73eb0744
Yves Bonjour <yves.bonjour@gmail.com>
2017-01-03 21:07:42 +0100

    Moves Execution Order link to test result list header

--- web/www/app/test-result/header/test-results-header.component.ts
+++ web/www/app/test-result/header/test-results-header.component.ts
@@ -1,12 +1,25 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
+import {RouteParams, ROUTER_DIRECTIVES} from "@angular/router-deprecated";
 
 @Component({
     selector: 'test-results-header',
     templateUrl: 'app/test-result/header/test-results-header.html',
-    styleUrls: ['app/test-result/header/test-results-header.css']
+    styleUrls: ['app/test-result/header/test-results-header.css'],
+    directives: [ROUTER_DIRECTIVES]
 })
-export class TestResultsHeaderComponent {
+export class TestResultsHeaderComponent implements OnInit {
     @Input() numPassedResults:number;
     @Input() numRetriedResults: number;
     @Input() numFailedResults: number;
+
+    testSuiteId: string;
+    runId: string;
+
+    constructor(
+        private _routeParams: RouteParams) {}
+
+    ngOnInit():void {
+        this.testSuiteId = this._routeParams.get('testsuite_id');
+        this.runId = this._routeParams.get('run_id')
+    }
 }
--- web/www/app/test-result/list/test-result-list.component.ts
+++ web/www/app/test-result/list/test-result-list.component.ts
@@ -16,7 +16,6 @@ import {RevisionListComponent} from "../../revision/revision-list.component";
 export class TestResultListComponent implements OnInit {
 	errorMessage: string;
 	runId: string;
-	testSuiteId: string;
 	passedResults: TestWithResults[] = [];
 	failedResults: TestWithResults[] = [];
 	retriedResults: TestWithResults[] = [];
@@ -27,7 +26,6 @@ export class TestResultListComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		this.testSuiteId = this._routeParams.get('testsuite_id');
 		this.runId = this._routeParams.get('run_id');
 		this.getResults(this.runId);
 	}
