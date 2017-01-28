a147f07a46e5817a46ff8ffade502344d733e14a
Yves Bonjour <yves.bonjour@gmail.com>
2017-01-28 15:03:58 +0100

    Renames new methods in history service

--- web/www/app/history/history.service.ts
+++ web/www/app/history/history.service.ts
@@ -7,26 +7,26 @@ import {Observable} from "rxjs/Rx";
 export class HistoryService {
     constructor(private _http: Http) {}
 
-    getHistoryNew(testsuite_id: string, limit: number): Observable<HistoryEntry[]> {
+    getHistory(testsuite_id: string, limit: number): Observable<HistoryEntry[]> {
         return this._http.get("/api/testsuites/" + testsuite_id + "/history-new?limit=" + limit)
-            .map(HistoryService.extractBodyNew)
+            .map(HistoryService.extractBody)
             .catch(HistoryService.extractError)
     }
 
-    private static extractBodyNew(response: Response): HistoryEntry[]{
+    private static extractBody(response: Response): HistoryEntry[]{
         if(response.status != 200) throw new Error("Bad response status: " + response.status);
 
-        return HistoryService.convertNewJsonToHistoryEntries(response.json());
+        return HistoryService.convertJsonToHistoryEntries(response.json());
     }
 
-    private static convertNewJsonToHistoryEntries(json: any): HistoryEntry[] {
+    private static convertJsonToHistoryEntries(json: any): HistoryEntry[] {
         let history = [];
         let testnames = json.testnames;
         for(var historyEntryJson of json.runHistory) {
             let historyEntry = new HistoryEntry();
             historyEntry.revision = historyEntryJson.revision;
             historyEntry.runId = historyEntryJson.runId;
-            historyEntry.results = HistoryService.convertNewJsonToResultMap(historyEntryJson.results2, testnames);
+            historyEntry.results = HistoryService.convertJsonToResultMap(historyEntryJson.results2, testnames);
 
             history.push(historyEntry);
         }
@@ -34,7 +34,7 @@ export class HistoryService {
         return history;
     }
 
-    private static convertNewJsonToResultMap(json: any, testnames: string[]): {[id: string]: string} {
+    private static convertJsonToResultMap(json: any, testnames: string[]): {[id: string]: string} {
         let results: {[id: string]: string} = {};
 
         for(var i in testnames) {
