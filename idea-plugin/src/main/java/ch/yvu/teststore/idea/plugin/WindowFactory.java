e767386e4965f4cb78d95a13eef51099212f2b43
Yves Bonjour <yves.bonjour@ergon.ch>
2017-02-05 10:27:30 +0100

    Adds empty panel for test-store idea plugin

---
+++ idea-plugin/src/main/java/ch/yvu/teststore/idea/plugin/WindowFactory.java
@@ -0,0 +1,15 @@
+package ch.yvu.teststore.idea.plugin;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class WindowFactory implements ToolWindowFactory {
+
+	@Override
+	public void createToolWindowContent(
+			@NotNull Project project, @NotNull ToolWindow toolWindow) {
+
+	}
+}
