713cc1228816f167fcdce122dae632794b027131
Yves Bonjour <yves.bonjour@ergon.ch>
2017-02-12 19:55:04 +0100

    idea-plugin: Uses apache http instead of commons http

--- idea-plugin/src/main/java/ch/yvu/teststore/idea/plugin/load/LoadTask.java
+++ idea-plugin/src/main/java/ch/yvu/teststore/idea/plugin/load/LoadTask.java
@@ -5,12 +5,18 @@ import static java.util.stream.Collectors.toList;
 import ch.yvu.teststore.idea.plugin.model.Model;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
-import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.List;
 
@@ -18,7 +24,7 @@ public abstract class LoadTask<V extends Model> extends Task.Backgroundable {
 
 	private List<V> results;
 	private LoadListener loadListener;
-	protected final HttpClient httpClient = new HttpClient();
+	protected final HttpClient httpClient = HttpClientBuilder.create().build();
 	private final String baseUrl;
 
 	public LoadTask(@Nls(capitalization = Nls.Capitalization.Title) @NotNull String title, String baseUrl) {
@@ -56,19 +62,36 @@ public abstract class LoadTask<V extends Model> extends Task.Backgroundable {
 	protected String getJson(String path) {
 		try {
 			URL url = new URL(new URL(baseUrl), path);
-			GetMethod get = new GetMethod(url.toString());
-			int status = httpClient.executeMethod(get);
-			if (status != 200) {
+			HttpGet get = new HttpGet(url.toString());
+
+
+			HttpResponse response = httpClient.execute(get);
+			int status = response.getStatusLine().getStatusCode();
+			if ( status != 200) {
 				System.err.println("ERROR: Unexpected status code (" + status + ")");
 				return null;
 			}
 
-			return new String(get.getResponseBody());
+			return readBody(response);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
+
+	private static String readBody(HttpResponse response) throws IOException {
+		BufferedReader rd = new BufferedReader(
+				new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+
+		return result.toString();
+	}
+
 
 	public interface LoadListener {
 
