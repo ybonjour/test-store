38340afc1c7e4f7bba5f25c8b4aa8d869783d795
Yves Bonjour <yves.bonjour@ergon.ch>
2019-04-12 10:49:37 +0200

    Executes build instead of assemble task for backend

--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -5,4 +5,4 @@ set -e
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 root=$dir/../..
 
-docker run -v $root/:/workspace --rm java:8-jdk bash -ce "(cd workspace/backend; ../gradlew assemble)"
+docker run -v $root/:/workspace --rm java:8-jdk bash -ce "(cd workspace/backend; ../gradlew build)"
