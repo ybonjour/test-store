3ca108d68bb39ab44694eef86e305d2136d2fabc
Martin Brandtner <martin.brandtner@ergon.ch>
2019-06-05 10:45:00 +0200

    Refactors backend/scripts/build.sh
    - Adds uid and gid mapping to avoid persmission mismatch (root vs local user) after run of script
    - Switches from deprecated java to openjdk image
    - Sets GRADLE_USER_HOME to avoid cryptic folders, such as '?', genereated by the Gradle wrapper

--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -5,4 +5,7 @@ set -e
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 root=$dir/../..
 
-docker run -v $root/:/workspace --rm java:8-jdk bash -ce "(cd workspace/backend; ../gradlew build)"
+uid=$(id -u)
+gid=$(id -g)
+
+docker run -v $root/:/workspace -u $uid:$gid -e GRADLE_USER_HOME=/workspace/work-directory --rm openjdk:8-jdk bash -ce "(cd workspace/backend; ../gradlew build)"
