95daf3f2efcdde2efb0f337e277e2b3765990cfd
Yves Bonjour <yves.bonjour@gmail.com>
2019-08-02 12:22:48 +0200

    Integrates new web into test-store under port 8080

--- web/scripts/build.sh
+++ web/scripts/build.sh
@@ -6,4 +6,5 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 root=$dir/../..
 
 docker run -v $root/web/www:/www --rm node bash -ce "(cd /www; npm install; npm run typings install; npm run tsc)"
+docker run -v $root/web/www2:/www2 --rm node bash -ce "(cd /www2; npm clean-install; npm run build)"
 
